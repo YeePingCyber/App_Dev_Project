2661f2b9b327172d38db67d7ddede040ca25ca4d
Sz3yan <sz3yan@gmail.com>
2021-12-23 10:55:45 +0800

    v0.3

---
+++ __init__.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
+import shelve
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("base.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
