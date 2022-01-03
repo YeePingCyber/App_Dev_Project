c5b635f55a7d54ec4b63a87f6f8b7c899a21230f
Sz3yan <sz3yan@gmail.com>
2022-01-03 10:27:34 +0800

    v1.1

--- __init__.py
+++ __init__.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template
 from flask import Flask, render_template, request, redirect, url_for
 import shelve
+from userCart import userCart
 
 app = Flask(__name__)
 
@@ -9,11 +10,15 @@ app = Flask(__name__)
 def home():
     return render_template("home.html")
 
-@app.route("/cartEmpty")
-def empty_cart():
-    return render_template("cart_empty.html")
 
-# test
+@app.route("/cart")
+def cart():
+    userCart()
+    if userCart.count == 0:
+        return render_template("cart_empty.html")
+
+    else:
+        return render_template("cart.html")
 
 
 if __name__ == "__main__":
